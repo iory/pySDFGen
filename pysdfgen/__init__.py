f2726e757c281191fef0223a539f7b742e05724e
iory <ab.ioryz@gmail.com>
2020-03-15 05:00:15 +0900

    Add obj2sdf

--- pysdfgen/__init__.py
+++ pysdfgen/__init__.py
@@ -2,6 +2,7 @@
 
 import pkg_resources
 import os.path as osp
+import subprocess
 
 
 __version__ = pkg_resources.get_distribution('pysdfgen').version
@@ -9,3 +10,19 @@ __version__ = pkg_resources.get_distribution('pysdfgen').version
 
 SDFGen_executable = osp.join(
     osp.abspath(osp.dirname(__file__)), 'SDFGen')
+
+
+def obj2sdf(obj_filepath, dim=100, padding=5):
+    obj_filepath = str(obj_filepath)
+    parent = osp.dirname(obj_filepath)
+    basename = osp.basename(obj_filepath)
+    stem, _ = osp.splitext(basename)
+    sdf_filepath = osp.join(parent, stem + ".sdf")
+    p = subprocess.Popen(
+        [SDFGen_executable,
+         str(obj_filepath),
+         str(dim),
+         str(padding)],
+        stdout=subprocess.DEVNULL)
+    p.wait()
+    return osp.join(parent, sdf_filepath)
