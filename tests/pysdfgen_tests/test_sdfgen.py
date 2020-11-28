7eabc91e334000e2b04351875f3aa6845b7b2bf9
Hirokazu Ishida <38597814+HiroIshida@users.noreply.github.com>
2020-11-29 01:16:58 +0900

    replace default path by specified path (#3)
    * replace default path by specified path
    
    * add test

--- tests/pysdfgen_tests/test_sdfgen.py
+++ tests/pysdfgen_tests/test_sdfgen.py
@@ -4,10 +4,14 @@ import unittest
 
 from pysdfgen import obj2sdf
 
-
 current_dir = osp.abspath(osp.dirname(__file__))
 bunny_objpath = osp.join(current_dir, 'data', 'bunny.obj')
 bunny_sdfpath = osp.join(current_dir, 'data', 'bunny.sdf')
+
+another_data_dir = osp.join(current_dir, "tmp")
+another_bunny_sdfpath = osp.join(another_data_dir, 'bunny.sdf')
+if not os.path.exists(another_data_dir):
+    os.makedirs(another_data_dir)
 
 
 class TestSDFGen(unittest.TestCase):
@@ -34,4 +38,18 @@ class TestSDFGen(unittest.TestCase):
         with self.assertRaises(OSError):
             obj2sdf(bunny_objpath,
                     output_filepath=bunny_sdfpath,
+                    overwrite=False)
+
+        # testing the case when a custom output path is specified
+        another_output_path = obj2sdf(bunny_objpath,
+                                      dim=dim,
+                                      output_filepath=another_bunny_sdfpath,
+                                      overwrite=True)
+        self.assertEqual(another_output_path, another_bunny_sdfpath)
+        self.assertTrue(os.path.exists(another_bunny_sdfpath))
+
+        with self.assertRaises(OSError):
+            obj2sdf(bunny_objpath,
+                    dim=dim,
+                    output_filepath=another_bunny_sdfpath,
                     overwrite=False)
